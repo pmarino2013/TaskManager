da5d152f41cd551b99c42af112498a0b9387cbb6
Pablo Marino <pmarino2013@gmail.com>
2023-09-08 08:48:30 -0300

    Los links de footer no llevan target porque deben abrirse en la misma pagina

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,52 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Components/Task/css/Footer.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Components/Task/css/Footer.css";
 
 const Footer = () => {
   return (
-<footer className="footer text-center">
-  <div className="container">
-    <div className="row">
-      <div className="col-md-4 col-sm-6 my-4">
-        <div className="footer-links">
-          <h4>Company</h4>
-          <ul className="my-list list-unstyled"> 
-          <li><Link to="/aboutUs" target="_blank">About Us</Link></li>
-            <li><Link to="/contact" target="_blank">Contact</Link></li>
-            <li><a href="/error404" target="_blank">Privacy Policy</a></li>
-            <li><a href="/error404" target="_blank">Affiliate</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="col-md-4 col-sm-6 my-4">
-        <div className="footer-links">
-          <h4>Help</h4>
-          <ul className="my-list list-unstyled"> {/* Agrega "list-unstyled" para eliminar los estilos de lista */}
-            <li><a href="/error404" target="_blank">FAQ</a></li>
-            <li><a href="/error404" target="_blank">Help Center</a></li>
-            <li><a href="/error404" target="_blank">Legal Notices</a></li>
-            <li><a href="/error404" target="_blank">Corporate Information</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="footer-links Follows-icons my-4">
-          <h4>Follow Us</h4>
-          <div className="social-icons">
-            <div className="social-link my-list">
-              <a href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook-f"></i></a>
-              <a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a>
-              <a href="https://twitter.com/?lang=en" target="_blank"><i className="fab fa-twitter"></i></a>
-              <a href="https://www.youtube.com" target="_blank"><i className="fab fa-youtube"></i></a>
+    <footer className="footer text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-sm-6 my-4">
+            <div className="footer-links">
+              <h4>Company</h4>
+              <ul className="my-list list-unstyled">
+                <li>
+                  <Link to="/aboutUs">About Us</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                  <a href="/error404">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="/error404">Affiliate</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-6 my-4">
+            <div className="footer-links">
+              <h4>Help</h4>
+              <ul className="my-list list-unstyled">
+                {" "}
+                {/* Agrega "list-unstyled" para eliminar los estilos de lista */}
+                <li>
+                  <a href="/error404">FAQ</a>
+                </li>
+                <li>
+                  <a href="/error404">Help Center</a>
+                </li>
+                <li>
+                  <a href="/error404">Legal Notices</a>
+                </li>
+                <li>
+                  <a href="/error404">Corporate Information</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="footer-links Follows-icons my-4">
+              <h4>Follow Us</h4>
+              <div className="social-icons">
+                <div className="social-link my-list">
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="https://www.instagram.com/" target="_blank">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                  <a href="https://twitter.com/?lang=en" target="_blank">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a href="https://www.youtube.com" target="_blank">
+                    <i className="fab fa-youtube"></i>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <p className="fs-6 text-center">&copy; 2023 - All Rights Reserved.</p>
-</footer>
-
+      <p className="fs-6 text-center">&copy; 2023 - All Rights Reserved.</p>
+    </footer>
   );
 };
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Nav, Row, Col, Button } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useLocation, Link } from 'react-router-dom';
-import Logo from '../img/Logo.png';
-import { logoutUser } from '../redux/Action/UserAction';
-import ModalPost from './Task/ModalPost';
-import AdminNavbar from '../Components/Admin/AdminNavbar';
+import React, { useEffect, useState } from "react";
+import { Nav, Row, Col, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import Logo from "../img/Logo.png";
+import { logoutUser } from "../redux/Action/UserAction";
+import ModalPost from "./Task/ModalPost";
+import AdminNavbar from "../Components/Admin/AdminNavbar";
 
-import '../Components/Task/css/Nav.css';
+import "../Components/Task/css/Nav.css";
 
 const Navbar = () => {
-  const userToken = sessionStorage.getItem('userToken');
+  const userToken = sessionStorage.getItem("userToken");
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
 
   const handleLogout = () => {
     dispatch(logoutUser());
-    sessionStorage.removeItem('userToken');
-    navigate('/');
+    sessionStorage.removeItem("userToken");
+    navigate("/");
   };
 
-  const isHomePage = location.pathname === '/';
-  const isAdminPage = location.pathname === '/adminPage';
+  const isHomePage = location.pathname === "/";
+  const isAdminPage = location.pathname === "/adminPage";
 
   const [showModal, setShowModal] = useState(false);
 
@@ -34,7 +34,7 @@ const Navbar = () => {
     setShowModal(false);
   };
 
-  const userName = sessionStorage.getItem('userName');
+  const userName = sessionStorage.getItem("userName");
 
   useEffect(() => {
     // Este useEffect se ejecutará cada vez que userName cambie en el sessionStorage
@@ -43,7 +43,7 @@ const Navbar = () => {
   }, [userName]); // Asegúrate de incluir userName como una dependencia
 
   const capitalizeFirstLetter = (string) => {
-    if (string && typeof string === 'string' && string.length > 0) {
+    if (string && typeof string === "string" && string.length > 0) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     } else {
       return string;
@@ -51,12 +51,23 @@ const Navbar = () => {
   };
 
   return (
-    <Nav activeKey="/" className="fixed-top  nav justify-content-between col-lg-12 col-md-12 col-sm-12">
+    <Nav
+      activeKey="/"
+      className="fixed-top  nav justify-content-between col-lg-12 col-md-12 col-sm-12"
+    >
       <Nav.Item className="mx-5">
         <Row className="d-flex align-items-center">
           <Col xs="12" className="text-center">
-            <Link className="d-flex align-items-center text-light text-decoration-none my-2">
-              <img src={Logo} alt="TaskGenius Logo" height="50" className="mr-3" />
+            <Link
+              className="d-flex align-items-center text-light text-decoration-none my-2"
+              to="/"
+            >
+              <img
+                src={Logo}
+                alt="TaskGenius Logo"
+                height="50"
+                className="mr-3"
+              />
               <h1 className="text-light Nav-title m-0">TaskGenius</h1>
             </Link>
           </Col>
@@ -65,21 +76,30 @@ const Navbar = () => {
 
       {userToken && !isHomePage && (
         <Nav.Item className="mx-auto">
-          <h1 className="text-light my-3 Nav-title">Welcome {capitalizeFirstLetter(userName)}</h1>
+          <h1 className="text-light my-3 Nav-title">
+            Welcome {capitalizeFirstLetter(userName)}
+          </h1>
         </Nav.Item>
       )}
 
       {userToken && !isHomePage && (
         <Nav.Item className="mr-5 d-flex">
-          <Button variant="danger" className="logout-button my-3 mr-2" onClick={handleLogout}>
+          <Button
+            variant="danger"
+            className="logout-button my-3 mr-2"
+            onClick={handleLogout}
+          >
             Logout
           </Button>
 
-          {isAdminPage ? 
-          (
+          {isAdminPage ? (
             <AdminNavbar />
           ) : (
-            <Button variant="primary" className="new-task-button my-3" onClick={openModal}>
+            <Button
+              variant="primary"
+              className="new-task-button my-3"
+              onClick={openModal}
+            >
               New Task
             </Button>
           )}
